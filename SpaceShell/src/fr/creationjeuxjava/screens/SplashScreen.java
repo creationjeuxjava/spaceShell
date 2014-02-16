2daabd3b4795129370444c6c0410ce090200960b
JohnCarmack <faouzy.chaib@gmail.com>
2014-02-16 13:29:36 +0100

    SplashScreen added

---
+++ SpaceShell/src/fr/creationjeuxjava/screens/SplashScreen.java
@@ -0,0 +1,43 @@
+package fr.creationjeuxjava.screens;
+
+import com.badlogic.gdx.Screen;
+
+//============================================================================
+// Name        : SplashScreen.java from SpaceShell
+// Author      : JohnCarmack
+// Version     : 
+// Copyright   : www.creationjeuxjava.fr !
+// Description : 
+// Date		   : 16 févr. 2014 at 13:25:55	
+//============================================================================
+public class SplashScreen implements Screen {
+
+	@Override
+	public void render(float delta) {
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void show() {
+	}
+
+	@Override
+	public void hide() {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+}

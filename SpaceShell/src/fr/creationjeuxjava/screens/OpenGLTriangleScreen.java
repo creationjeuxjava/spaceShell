fc0d7d0bdeb1fed62c1481f3e42320e95acb907a
JohnCarmack <creationjeuxjava@gmail.com>
2014-02-27 22:03:10 +0100

    TriangleScreen test

--- SpaceShell/src/fr/creationjeuxjava/screens/OpenGLTriangleScreen.java
+++ SpaceShell/src/fr/creationjeuxjava/screens/OpenGLTriangleScreen.java
@@ -1,6 +1,16 @@
 package fr.creationjeuxjava.screens;
 
+import test.openGL.MeshHelper;
+
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.FPSLogger;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
 //============================================================================
@@ -13,8 +23,54 @@ import com.badlogic.gdx.Screen;
 //============================================================================
 public class OpenGLTriangleScreen implements Screen {
 
+	@Mesh myMesh;
+	SpriteBatch batch;
+	BitmapFont bitmapfont;
+	MeshHelper meshHelper;
+	FPSLogger fps;
+
 	@Override
-	public void render(float delta) {
+	public void create() {
+
+		// out(Gdx.gl.glGetString(GL20.GL_VERSION));
+		// out(Gdx.gl.glGetString(GL20.GL_RENDERER));
+		batch = new SpriteBatch();
+		bitmapfont = new BitmapFont();
+		bitmapfont.setColor(Color.RED);
+		meshHelper = new MeshHelper();
+		meshHelper
+				.createMesh(new float[] { -0.5f, -0.5f, 0.5f, -0.5f, 0f, 0.5f });
+
+	}
+
+	@Override
+	public void render() {
+		Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+		Gdx.gl.glEnable(GL20.GL_BLEND_SRC_ALPHA);
+
+		batch.begin();
+
+		bitmapfont.draw(batch,
+				"Version OPENGL : " + Gdx.gl.glGetString(GL20.GL_VERSION), 6,
+				291);
+		bitmapfont.draw(batch,
+				"Graphics CARD : " + Gdx.gl.glGetString(GL20.GL_RENDERER), 6,
+				305);
+		bitmapfont.draw(batch,
+				"OpenGL 2.0 ? : " + Gdx.graphics.isGL20Available(), 6, 321);
+
+		bitmapfont.draw(batch, "JAVA HEAP : " + Gdx.app.getJavaHeap() / 10E4,
+				6, 191);
+		bitmapfont.draw(batch, "FPS : " + Gdx.graphics.getFramesPerSecond(), 6,
+				210);
+		// meshHelper.drawMesh();
+		batch.end();
+
+	}
+
+	public static void out(String s) {
+		System.out.println(s);
 	}
 
 	@Override
@@ -22,24 +78,24 @@ public class OpenGLTriangleScreen implements Screen {
 	}
 
 	@Override
-	public void show() {
-	}
-
-	@Override
-	public void hide() {
-	}
-
-	@Override
 	public void pause() {
+		out("Paused!");
+		bitmapfont.setColor(Color.YELLOW);
+
 	}
 
 	@Override
 	public void resume() {
+		out("Resumed!");
+		bitmapfont.setColor(Color.BLUE);
 	}
 
 	@Override
 	public void dispose() {
-	}
+		batch.dispose();
+		bitmapfont.dispose();
+		meshHelper.dispose();
 
+	}
 }
 
